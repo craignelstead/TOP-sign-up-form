f5be7c734f80f31afef513b3331234cdcf3ee212
craignelstead <ctnelst92@gmail.com>
2023-08-22 22:20:46 -0600

    Added valid and invalid password classes and function that checks for match. Added function that adds dashes to phone numbers

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ listenForPWconfirm();
 
 //Add event listener:
 function listenForPWconfirm() {
-    let confirmPW = document.getElementById('passwordconfirm');
-    confirmPW.addEventListener("input", checkIfMatch);
+    const confirmPW = document.getElementById('passwordconfirm');
+    confirmPW.addEventListener('input', checkIfMatch);
+
+    const password = document.getElementById('password');
+    password.addEventListener('input', checkIfMatch);
+
+    const phoneNum = document.getElementById("phone");
+    phoneNum.addEventListener('input', phoneHyphen);
 }
 
 //Checks to see if passwords match
@@ -15,15 +21,50 @@ function checkIfMatch() {
     let pass1 = document.getElementById('password');
     let pass2 = document.getElementById('passwordconfirm');
 
-    (pass1.textContent === pass2.textContent) ? passMatch() : passFail();
+    (pass1.value === pass2.value) ? 
+        passMatch(pass1, pass2) : 
+        passFail(pass1, pass2);
 }
 
 //Style based on passwords matching
-function passMatch() {
-    alert('Yes');
+function passMatch(pass1, pass2) {
+    console.log('pass');
+    pass1.classList.remove('invalid');
+    pass1.classList.add('valid');
+    pass2.classList.remove('invalid');
+    pass2.classList.add('valid');
+
+    //Removes invalid message
+    const validMsg = '';
+    const match = document.getElementById('mismatchMsg');
+    match.textContent = validMsg;
 }
 
 //Style based on password NOT matching
-function passFail() {
-    alert('No');
+function passFail(pass1, pass2) {
+    console.log('fail');
+    pass1.classList.remove('valid');
+    pass1.classList.add('invalid');
+    pass2.classList.remove('valid');
+    pass2.classList.add('invalid');
+
+    //Adds message when passwords do not match
+    const invalidMsg = 'Passwords do not match';
+    const mismatch = document.getElementById('mismatchMsg');
+    mismatch.textContent = invalidMsg;
+}
+
+//Automatically add hyphen to phone number
+function phoneHyphen() {
+    const phoneNum = document.getElementById("phone");
+
+    let phoneArray = phoneNum.value.split('');
+    let phoneLn = phoneNum.value.length;
+
+    if (phoneLn === 3 || phoneLn === 7) phoneArray.push('-');
+
+    let newPhone = phoneArray.join().replaceAll(',','');
+    console.log(newPhone);
+
+    phoneNum.value = newPhone;
 }
